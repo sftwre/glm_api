4b1c53daecda01b7caa9276715d2cf0f1e5142c3
Isaac Buitrago <mightyking7@gmail.com>
2020-08-30 23:28:21 -0500

    Loaded test data, passed first row to API, and added assertions for expected output.

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,6 +1,12 @@
 import pytest
-from api import app
-from flask import Response
+from api import app, ROOT_DIR
+import pandas as pd
+
+def load_test_data():
+    test_file = f'{ROOT_DIR}/exercise_26_test.csv'
+    df_test = pd.read_csv(test_file)
+    return df_test
+
 
 @pytest.fixture
 def client():
@@ -10,8 +16,33 @@ def client():
         yield client
 
 def test_empty(client):
-    """Start with a blank database."""
-
+    """Test that the api is reachable"""
     rv = client.get('/')
 
     assert rv.status_code == 200
+
+def test_one_row(client):
+    """
+    Tests that the /predict route can successfully accept one row
+    of parameters and return three variables : business_outcome, phat, and params.
+    The output must be in JSON format.
+    :param client: connection to api
+    """
+    df_test = load_test_data()
+
+    # get first row
+    data = df_test.iloc[0].to_dict()
+
+    rv = client.post('/predict', json=data)
+
+    assert rv.status_code == 200
+
+    assert rv.is_json == True
+
+    jsonRes = rv.get_json()
+
+    assert 'business_outcome' in jsonRes
+    assert 'phat' in jsonRes
+    assert 'params' in jsonRes
+
+    # print(json)
