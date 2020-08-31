893538e503f9a40ead80080dbaf045fcb40cfd48
Isaac Buitrago <mightyking7@gmail.com>
2020-08-31 04:31:00 -0500

    Converted load_test_data into fixture, implemented common assertions for each row of output into seperate function, created test for five rows and 10,000 rows of data.

--- tests/test_api.py
+++ tests/test_api.py
@@ -2,10 +2,11 @@ import pytest
 from api import app, ROOT_DIR
 import pandas as pd
 
-def load_test_data():
+@pytest.fixture
+def df_test():
     test_file = f'{ROOT_DIR}/exercise_26_test.csv'
     df_test = pd.read_csv(test_file)
-    return df_test
+    yield df_test
 
 
 @pytest.fixture
@@ -15,21 +16,36 @@ def client():
     with app.test_client() as client:
         yield client
 
-def test_empty(client):
+def test_home(client):
     """Test that the api is reachable"""
     rv = client.get('/')
 
     assert rv.status_code == 200
 
-def test_one_row(client):
+def assert_row(json):
+    """
+    Asserts that a row from the response payload is of the correct type and
+    contains the appropriate data.
+    :param json: Row of output
+    """
+    # ensure correct variables were returned
+    assert 'business_outcome' in json
+    assert 'phat' in json
+    assert 'params' in json
+
+    # ensure variables are of the correct type
+    assert type(json['business_outcome']) == int
+    assert type(json['phat']) == float
+    assert type(json['params']) == dict
+
+
+def test_one_row(client, df_test):
     """
     Tests that the /predict route can successfully accept one row
-    of parameters and return three variables : business_outcome, phat, and params.
+    of test data and return three variables : business_outcome, phat, and params.
     The output must be in JSON format.
     :param client: connection to api
     """
-    df_test = load_test_data()
-
     # get first row
     data = df_test.iloc[0].to_dict()
 
@@ -41,12 +57,56 @@ def test_one_row(client):
 
     jsonRes = rv.get_json()
 
-    assert 'business_outcome' in jsonRes
-    assert 'phat' in jsonRes
-    assert 'params' in jsonRes
+    assert_row(jsonRes)
 
-    assert type(jsonRes['business_outcome']) == int
-    assert type(jsonRes['phat']) == float
-    assert type(jsonRes['params'][0]) == dict
 
-    # print(json)
+def test_five_rows(client, df_test):
+    """
+    Tests that the /predict route can successfully accept five rows
+    of test data and return an array with the business_outcome, phat,
+    and params variables in JSON.
+    :param client: connection to api
+    """
+    nRows = 5
+
+    # get first five row
+    data = df_test.head().to_dict('records')
+
+    rv = client.post('/predict', json=data)
+
+    assert rv.status_code == 200
+
+    assert rv.is_json == True
+
+    jsonRes = rv.get_json()
+
+    assert len(jsonRes) == nRows
+
+    for obj in jsonRes:
+        assert_row(obj)
+
+def test_all_rows(client, df_test):
+    """
+    Tests that the /predict route can successfully accept five rows
+    of test data and return an array with the business_outcome, phat,
+    and params variables in JSON.
+    :param client: connection to api
+    """
+
+    nRows = df_test.shape[0]
+
+    # get first five row
+    data = df_test.to_dict('records')
+
+    rv = client.post('/predict', json=data)
+
+    assert rv.status_code == 200
+
+    assert rv.is_json == True
+
+    jsonRes = rv.get_json()
+
+    assert len(jsonRes) == nRows
+
+    for obj in jsonRes:
+        assert_row(obj)
